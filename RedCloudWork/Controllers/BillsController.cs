867ebd7a7af1cf68cc3b346f49a26ab6e93b0b90
ta_wuhen <781739091@qq.com>
2015-11-25 22:26:38 +0800

    add upload

--- RedCloudWork/Controllers/BillsController.cs
+++ RedCloudWork/Controllers/BillsController.cs
@@ -1,16 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.Entity;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Helpers;
 using System.Web.Mvc;
+using NPOI.HSSF.UserModel;
 using RedCloudWork.Domian;
 
 namespace RedCloudWork.Controllers
 {
     public class BillsController : Controller
     {
+        protected List<Bills> GetBillsByExecl()
+        {
+            return new List<Bills>();
+        }
        
         // GET: Bills
         public ActionResult AddBills()
@@ -29,9 +36,21 @@ namespace RedCloudWork.Controllers
         [HttpPost]
         public JsonResult AddBills(string name, HttpPostedFileBase file)
         {
+            if (Request.Files[0] !=null)
+            {
+                var fileName = Path.Combine(Request.MapPath("~/Upload"), Path.GetFileName(Request.Files[0].FileName));
+                try
+                {
+                    file.SaveAs(fileName);
+                }
+                catch (Exception ex)
+                {
+                    return Json(new { state = "no" });
+                }
+            }
 
-            var result = new {state="ok"};
-            return  Json(result);
+            var result = new { state = "ok" };
+            return Json(result);
         }
     }
 }
